024272ad55538d28d999b988944519418d1fa6ce
jburba <jacob_burba@brown.edu>
2022-05-19 15:02:48 -0400

    tanh tapered airy beam

--- Linalg/healpix.py
+++ Linalg/healpix.py
@@ -70,6 +70,16 @@ class Healpix(HEALPix):
         maximum of the beam in degrees.
     diam : float, optional
         Antenna (aperture) diameter in meters.  Used if ``beam_type = 'airy'``.
+    cosfreq : float, optional
+        Cosine frequency in inverse radians.  Used if
+        ``beam_type = 'gausscosine'``.
+    tanh_freq : float, optional
+        Exponential frequency (rate parameter) in inverse radians.  Used if
+        ``beam_type = 'tanhairy'``.
+    tanh_sl_red : float, optional
+        Airy sidelobe amplitude reduction as a fractional percent.  For
+        example, passing 0.99 reduces the sidelobes by 0.01, i.e. two orders
+        of magnitude.  Used if ``beam_type = 'tanhairy'``.
 
     """
     def __init__(
@@ -87,7 +97,9 @@ class Healpix(HEALPix):
             peak_amp=1.0,
             fwhm_deg=None,
             diam=None,
-            cosfreq=None
+            cosfreq=None,
+            tanh_freq=None,
+            tanh_sl_red=None
             ):
         # Use HEALPix as parent class to get
         # useful astropy_healpix functions
@@ -176,10 +188,11 @@ class Healpix(HEALPix):
             if not '.' in str(beam_type):
                 beam_type = beam_type.lower()
                 allowed_types = [
-                    'uniform', 'gaussian', 'airy', 'gausscosine', 'taperairy'
+                    'uniform', 'gaussian', 'airy', 'gausscosine', 'taperairy',
+                    'tanhairy'
                 ]
                 assert beam_type in allowed_types, \
-                    "Only uniform, Gaussian, and Airy beams are supported."
+                    f"Only {', '.join(allowed_types)} beams are supported."
                 self.beam_type = beam_type
                 self.uvb = None
             else:
@@ -211,16 +224,35 @@ class Healpix(HEALPix):
         self.peak_amp = peak_amp
 
         if beam_type == 'gaussian':
-            assert diam is not None or fwhm_deg is not None, \
-                "If using a Gaussian beam, must also pass either " \
+            required_params = [diam, fwhm_deg]
+            assert self._check_required_params(required_params, all_req=False),\
+                "If using a Gaussian beam, must pass either " \
                 "'fwhm_deg' or 'diam'."
         elif beam_type == 'airy':
-            assert diam is not None or fwhm_deg is not None, \
-                "If using an Airy beam, must also pass either " \
+            required_params = [diam, fwhm_deg]
+            assert self._check_required_params(required_params, all_req=False),\
+                "If using an Airy beam, must pass either " \
                 "'fwhm_deg' or 'diam'."
+        elif beam_type == 'taperairy':
+            required_params = [diam, fwhm_deg]
+            assert self._check_required_params(required_params), \
+                "If using a taperairy beam, must pass " \
+                "'diam' and 'fwhm_deg'."
+        elif beam_type == 'gausscosine':
+            required_params = [fwhm_deg, cosfreq]
+            assert self._check_required_params(required_params), \
+                "If using a gausscosine beam, must pass " \
+                "'fwhm_deg' and 'cosfreq'."
+        elif beam_type == 'tanhairy':
+            required_params = [diam, tanh_freq, tanh_sl_red]
+            assert self._check_required_params(required_params), \
+                "If using a tanhairy beam, must pass " \
+                "'diam', 'tanh_freq', and 'tanh_sl_red'."
         self.fwhm_deg = fwhm_deg
         self.diam = diam
         self.cosfreq = cosfreq
+        self.tanh_freq = tanh_freq
+        self.tanh_sl_red = tanh_sl_red
 
         # Pixel filters
         pix_eor, ra_eor, dec_eor = self.get_pixel_filter(
@@ -251,7 +283,6 @@ class Healpix(HEALPix):
         # HEALPix pixel index arrays.  This mask allows you to take a set of
         # pixel values for the EoR model and propagate them into the FG model.
         self.eor_to_fg_pix = np.in1d(self.pix_fg, self.pix_eor)
-            
 
     def get_pixel_filter(
             self, fov_ra, fov_dec, return_radec=False, inverse=False):
@@ -453,6 +484,12 @@ class Healpix(HEALPix):
                 * self._gaussian_za(za, stddev_rad, self.peak_amp)
             )
         
+        elif self.beam_type == 'tanhairy':
+            beam_vals = (
+                self._airy_disk(za, self.diam, freq)
+                * self._tanh_taper(za, self.tanh_freq, self.tanh_sl_red)
+            )
+        
         elif self.beam_type == 'uvbeam':
             beam_vals, _ = self.uvb.interp(
                 az_array=az, za_array=za, freq_array=np.array([freq]),
@@ -550,6 +587,30 @@ class Healpix(HEALPix):
         beam_vals[nz] = 2. * j1(xvals[nz]) / xvals[nz]
         beam_vals[ze] = 1.
         return beam_vals ** 2
+    
+    def _tanh_taper(self, za, tanh_freq, tanh_sl_red):
+        """
+        Calculate a tanh tapering function.
+
+        Parameters
+        ----------
+        za : np.ndarray of floats
+            Zenith angle of each pixel in radians.
+        tanh_freq : float, optional
+            Exponential frequency (rate parameter) in inverse radians.
+        tanh_sl_red : float, optional
+            Airy sidelobe amplitude reduction as a fractional percent.  For
+            example, passing 0.99 reduces the sidelobes by 0.01, i.e. two
+            orders of magnitude.
+        
+        Returns
+        -------
+        taper_vals : np.ndarray
+            Array of tanh taper amplitudes for each zenith angle in `za`.
+
+        """
+        taper_vals = 1 - tanh_sl_red * np.tanh(tanh_freq * za)
+        return taper_vals
 
     def _fwhm_to_diam(self, fwhm, freq):
         """
@@ -610,3 +671,21 @@ class Healpix(HEALPix):
         sigma = np.arcsin(scalar * wavelength / (np.pi * diam))
         sigma *= np.sqrt(2) / 2.355
         return sigma
+
+    def _check_required_params(self, required_params, all_req=True):
+        """
+        Check if params in required_params are not None.
+
+        Parameters
+        ----------
+        required_params : iterable
+            Iterable of param values.
+        all_req : bool
+            If True, require all params are not None.  Otherwise, require
+            that only one param is not None. Defaults to True.
+
+        """
+        if not all_req:
+            return np.any([p is not None for p in required_params])
+        else:
+            return np.all([p is not None for p in required_params])
