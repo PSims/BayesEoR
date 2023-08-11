23e064f2ef0b27c81803888f28d0af6d7b13d403
Jacob Burba <jacob_burba@brown.edu>
2023-08-11 15:51:00 +0100

    Try uvtools approach to versioning

--- bayeseor/__init__.py
+++ bayeseor/__init__.py
@@ -1,20 +1,12 @@
-
 from importlib.metadata import PackageNotFoundError, version
-from pathlib import Path
-
-from setuptools_scm import get_version
 
 try:
-    # get accurate version for developer installs
-    version_str = get_version(Path(__file__).parent.parent)
-    __version__ = version_str
-
-except (LookupError, ImportError):
+    from ._version import version as __version__
+except ModuleNotFoundError:  # pragma: no cover
     try:
-        # Set the version automatically from the package details.
-        __version__ = version("pyuvsim")
-    except PackageNotFoundError:  # pragma: nocover
+        __version__ = version("uvtools")
+    except PackageNotFoundError:
         # package is not installed
-        pass
+        __version__ = "unknown"
 
 from . import matrices, model, params, posterior, utils
