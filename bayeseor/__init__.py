28f31678f8016750b72c2e011b38b826dcb4386f
Jacob Burba <jacob_burba@brown.edu>
2023-08-11 15:40:32 +0100

    Set __version__ in bayeseor/__init__.py; add setuptools as dependency for version checking

--- bayeseor/__init__.py
+++ bayeseor/__init__.py
@@ -1,1 +1,20 @@
+
+from importlib.metadata import PackageNotFoundError, version
+from pathlib import Path
+
+from setuptools_scm import get_version
+
+try:
+    # get accurate version for developer installs
+    version_str = get_version(Path(__file__).parent.parent)
+    __version__ = version_str
+
+except (LookupError, ImportError):
+    try:
+        # Set the version automatically from the package details.
+        __version__ = version("pyuvsim")
+    except PackageNotFoundError:  # pragma: nocover
+        # package is not installed
+        pass
+
 from . import matrices, model, params, posterior, utils
