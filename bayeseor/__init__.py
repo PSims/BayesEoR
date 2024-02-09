aaedb1e4ff7c63eac2400c0f509771eb246512bc
jburba <jacob.burba@manchester.ac.uk>
2024-02-09 16:58:17 +0000

    Fix __version__ attribute; accidentally left code copied from uvtools

--- bayeseor/__init__.py
+++ bayeseor/__init__.py
@@ -4,7 +4,7 @@ try:
     from ._version import version as __version__
 except ModuleNotFoundError:  # pragma: no cover
     try:
-        __version__ = version("uvtools")
+        __version__ = version("bayeseor")
     except PackageNotFoundError:
         # package is not installed
         __version__ = "unknown"
