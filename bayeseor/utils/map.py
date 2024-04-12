c177e80de50f006e2242f6c50fa4aa219a24fa83
jburba <jacob.burba@manchester.ac.uk>
2024-04-12 16:31:31 +0100

    Add noise generation, Hermitian checks, and add more attributes to MaximumAPosteriori class on init

--- bayeseor/utils/map.py
+++ bayeseor/utils/map.py
@@ -7,13 +7,16 @@ import matplotlib.pyplot as plt
 
 from ..matrices import BuildMatrixTree
 from ..model import (
+    load_inst_model,
     generate_k_cube_in_physical_coordinates,
     mask_k_cube,
     generate_k_cube_model_spherical_binning,
-    calc_mean_binned_k_vals
+    calc_mean_binned_k_vals,
+    generate_data_and_noise_vector_instrumental
 )
-from ..params import calculate_derived_params
+from ..params import calculate_derived_params, parse_uprior_inds
 from ..posterior import PowerSpectrumPosteriorProbability
+from .utils import mpiprint, vector_is_hermitian
 
 class MaximumAPosteriori(object):
     """
@@ -46,6 +49,9 @@ class MaximumAPosteriori(object):
         args = calculate_derived_params(cl_args)
 
         # Calculate k bin values
+        mpiprint(
+            "\nSpherical k bins:", style="bold", rank=1-int(verbose)
+        )
         mod_k = generate_k_cube_in_physical_coordinates(
             args.nu,
             args.nv,
@@ -59,38 +65,149 @@ class MaximumAPosteriori(object):
             mod_k_vo,
             args.ps_box_size_para_Mpc
         )[0]
-        k_vals = calc_mean_binned_k_vals(
+        self.k_vals = calc_mean_binned_k_vals(
             mod_k_vo,
             k_cube_voxels_in_bin,
             rank=1-int(verbose)
         )
 
         # Load matrices
-        bm = BuildMatrixTree(
+        self.bm = BuildMatrixTree(
             array_dir.as_posix(),
             args.include_instrumental_effects,
             args.use_sparse_matrices
         )
-        Ninv = bm.read_data(array_dir / "Ninv.npz", "Ninv")
-        T = bm.read_data(array_dir / "T.h5", "T")
-        T_Ninv_T = bm.read_data(array_dir / "T_Ninv_T.h5", "T_Ninv_T")
+        Ninv = self.bm.read_data((array_dir / "Ninv.npz").as_posix(), "Ninv")
+        self.Ndata = Ninv.shape[0]
+        T = self.bm.read_data((array_dir / "T.h5").as_posix(), "T")
+        T_Ninv_T = self.bm.read_data(
+            (array_dir / "T_Ninv_T.h5").as_posix(), "T_Ninv_T"
+        )
 
-        d = np.load(data_path)
-        Ninv_d = np.dot(Ninv, d)
-        d_Ninv_d = np.dot(d.conjugate(), Ninv_d)
+        data = np.load(data_path, allow_pickle=True)
+        if data.dtype.kind == "O":
+            # Data and noise vector saved in a single dictionary
+            dict_format = True
+            data = data.item()
+            if "noise" in data and "sigma" not in args:
+                gen_noise = False
+            else:
+                gen_noise = True
+        else:
+            # Data and noise vector saved as separate arrays
+            dict_format = False
+            if "noise_data_path" in args:
+                gen_noise = False
+            else:
+                gen_noise = True
+        if gen_noise:
+            effective_noise = None
+        else:
+            if dict_format:
+                effective_noise = data["noise"]
+            else:
+                effective_noise = np.load(args.noise_data_path)
+            args.sigma = effective_noise.std()
+        if args.include_instrumental_effects:
+            uvw_array_m, bl_red_array, phasor_vec = load_inst_model(
+                args.inst_model
+            )
+            if phasor_vec is not None and args.drift_scan:
+                phasor_vec = None
+            bl_red_array = bl_red_array*0 + bl_red_array.min()
+            avg_bl_red = np.mean(bl_red_array)
+            if gen_noise:
+                args.sigma *= avg_bl_red**0.5
+            if dict_format:
+                d_eor = data["data"]
+            else:
+                d_eor = data
+            if gen_noise:
+                d, effective_noise, bl_conj_map = \
+                    generate_data_and_noise_vector_instrumental(
+                        args.sigma,
+                        d_eor,
+                        args.nf,
+                        args.nt,
+                        uvw_array_m[0],
+                        bl_red_array[0],
+                        random_seed=args.noise_seed,
+                        rank=1-int(verbose)
+                    )
+            else:
+                d = d_eor.copy()
+                _, _, bl_conj_map =\
+                    generate_data_and_noise_vector_instrumental(
+                        0,
+                        d_eor,
+                        args.nf,
+                        args.nt,
+                        uvw_array_m[0],
+                        bl_red_array[0],
+                        rank=1-int(verbose)
+                    )
+        mpiprint(
+            "\nHermitian symmetry checks:", style="bold", rank=1-int(verbose)
+        )
+        mpiprint(
+            "signal is Hermitian:",
+            vector_is_hermitian(
+                d_eor, bl_conj_map, args.nt, args.nf, uvw_array_m.shape[1]
+            ),
+            rank=1-int(verbose)
+        )
+        mpiprint(
+            "signal + noise is Hermitian:",
+            vector_is_hermitian(
+                d, bl_conj_map, args.nt, args.nf, uvw_array_m.shape[1]
+            ),
+            rank=1-int(verbose)
+        )
+
+        mpiprint("\nSNR:", style="bold", rank=1-int(verbose))
+        mpiprint(f"Stddev(signal) = {d_eor.std():.4e}", rank=1-int(verbose))
+        effective_noise_std = effective_noise.std()
+        mpiprint(
+            f"Stddev(noise) = {effective_noise_std:.4e}",
+            rank=1-int(verbose)
+        )
+        mpiprint(
+            f"SNR = {(d_eor.std() / effective_noise_std):.4e}\n",
+            rank=1-int(verbose)
+        )
+
+        Ninv_d = Ninv * d  # Ninv is sparse, so we use * instead of np.dot
+        self.d_Ninv_d = np.dot(d.conjugate(), Ninv_d)
         dbar = np.dot(T.conjugate().T, Ninv_d)
+        nDims = len(k_cube_voxels_in_bin)
+        if args.use_intrinsic_noise_fitting:
+            nDims += 1
+        if args.use_LWM_Gaussian_prior:
+            nDims += 3
+        if args.include_instrumental_effects:
+            block_T_Ninv_T = []
         
+        if args.uprior_bins != "":
+            args.uprior_inds = parse_uprior_inds(args.uprior_bins, nDims)
+            mpiprint(
+                "\nUniform prior k-bin indices:",
+                f"{np.where(args.uprior_inds)[0]}\n",
+                rank=1-int(verbose)
+            )
+        else:
+            args.uprior_inds = None
+
         self.pspp = PowerSpectrumPosteriorProbability(
             T_Ninv_T,
             dbar,
-            k_vals,
+            self.k_vals,
             k_cube_voxels_in_bin,
             args.nuv,
             args.neta,
             args.nf,
             args.nq,
             Ninv,
-            d_Ninv_d,
+            self.d_Ninv_d,
             args.redshift,
             args.ps_box_size_ra_Mpc,
             args.ps_box_size_dec_Mpc,
@@ -100,7 +217,7 @@ class MaximumAPosteriori(object):
             uprior_inds=args.uprior_inds,
             inverse_LW_power=args.inverse_LW_power,
             dimensionless_PS=True,  # Currently only support \Delta^2(k)
-            block_T_Ninv_T=None,
+            block_T_Ninv_T=block_T_Ninv_T,
             intrinsic_noise_fitting=args.use_intrinsic_noise_fitting,
             use_shg=args.use_shg,
             rank=0,
@@ -109,6 +226,13 @@ class MaximumAPosteriori(object):
         )
 
         self.args = args
+        self.d = d
+        self.d_eor = d_eor
+        if gen_noise:
+            self.noise = effective_noise
+        self.T = T
+        self.uvw_array_m = uvw_array_m
+        self.bl_red_array = bl_red_array
     
     def map_estimate(
         self,
@@ -136,23 +260,44 @@ class MaximumAPosteriori(object):
         
         Returns
         -------
-        map_estimate : ndarray
+        map_coeffs : ndarray
             MAP coefficients of the model.
+        map_vis : ndarray
+            MAP model visibilities.
+        log_post : float
+            Log posterior probability.
         prior_cov : ndarray
-            Diagonal of the prior covariance matrix.
+            Diagonal of the prior covariance matrix.  Only returned if
+            `return_prior_cov` is True.
+        
+        Notes
+        -----
+        * This function does not currently support noise fitting
 
         """
         dmps = self._calculate_dmps(ps=ps, dmps=dmps)
-        map_estimate, _, prior_cov, _ = self.pspp.calc_SigmaI_dbar_wrapper(
-            dmps,
-            self.pspp.T_Ninv_T,
-            self.pspp.dbar
+        map_coeffs, dbar_SigmaI_dbar, prior_cov, log_det_Sigma = \
+            self.pspp.calc_SigmaI_dbar_wrapper(
+                dmps,
+                self.pspp.T_Ninv_T,
+                self.pspp.dbar
+            )
+        map_vis = np.dot(self.T, map_coeffs)
+
+        log_det_prior_cov = -1 * np.sum(np.log(prior_cov)).real
+        log_post = (
+            -0.5*log_det_Sigma
+            - 0.5*log_det_prior_cov
+            + 0.5*dbar_SigmaI_dbar
         )
+        if self.pspp.uprior_inds is not None:
+            log_post += np.sum(np.log(dmps[self.pspp.uprior_inds]))
+        log_post = log_post.real
 
         if not return_prior_cov:
-            return map_estimate
+            return map_coeffs, map_vis, log_post
         else:
-            return map_estimate, prior_cov
+            return map_coeffs, map_vis, log_post, prior_cov
 
     def calculate_prior_covariance(
         self,
