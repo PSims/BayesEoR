d97afdfe5ddab5eb43a484916776514eefc66d49
jburba <jacob.burba@manchester.ac.uk>
2025-02-19 11:58:21 +0000

    Session fixtures for configuration and matrices; Add a few tests of build matrix functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,93 @@ from pathlib import Path
 import pytest
 
 from bayeseor.params import BayesEoRParser
+from bayeseor.matrices import BuildMatrices
+from bayeseor.model import load_inst_model
+
+@pytest.fixture(scope="session")
+def config():
+    parser = BayesEoRParser()
+    args = parser.parse_args(["--config", "tests-config.yaml"])
+
+    # Add additional attributes required by a broad range of tests
+    uvw_array_m, bl_red_array, phasor_vec = load_inst_model(args.inst_model)
+    uvw_array_m_vec = np.reshape(uvw_array_m, (-1, 3))
+    args.uvw_array_m = uvw_array_m
+    args.n_vis = len(uvw_array_m_vec)
+    args.bl_red_array = bl_red_array
+    args.phasor_vec = phasor_vec
+    
+    yield args
+
+    del args
+
+
+@pytest.fixture(scope="session")
+def build_matrices(config):
+    cl_args = config
+
+    args = [
+        args.array_dir,
+        args.include_instrumental_effects,
+        args.use_sparse_matrices,
+        args.nu,
+        args.nv,
+        args.n_vis,
+        args.neta,
+        args.nf,
+        args.nu_min_MHz,
+        args.channel_width_MHz,
+        args.nq,
+        args.nt,
+        args.integration_time_seconds,
+        args.sigma,
+        args.fit_for_monopole,
+    ]
+
+    required_params = [
+        'nside',
+        'central_jd',
+        'telescope_latlonalt',
+        'drift_scan_pb',
+        'beam_type',
+        'beam_peak_amplitude',
+        'beam_center',
+        'fwhm_deg',
+        'antenna_diameter',
+        'cosfreq',
+        'achromatic_beam',
+        'beam_ref_freq',
+        'du_eor',
+        'dv_eor',
+        'du_fg',
+        'dv_fg',
+        'deta',
+        'fov_ra_eor',
+        'fov_dec_eor',
+        'nu_fg',
+        'nv_fg',
+        'npl',
+        'beta',
+        'fov_ra_fg',
+        'fov_dec_fg',
+        'simple_za_filter',
+        'uvw_array_m',
+        'bl_red_array',
+        'bl_red_array_vec',
+        'phasor_vec',
+        'use_shg',
+        'fit_for_shg_amps',
+        'nu_sh',
+        'nv_sh',
+        'nq_sh',
+        'npl_sh',
+        'effective_noise',
+        'taper_func'
+    ]
+    kwargs = {key: cl_args.__dict__[key] for key in required_params}
+
+    BM = BuildMatrices(*args, **kwargs)
+
+    yield BM
+
+    del BM
---
+++ tests/matrices/test_build_matrices.py
@@ -0,0 +1,78 @@
+import numpy as np
+from threadpoolctl import threadpool_info, threadpool_limits
+
+
+def test_build_Finv(build_matrices):
+    BM = build_matrices
+
+    BM.build_Finv()
+    Finv = BM.read_data(BM.array_save_directory, "Finv")
+
+    Finv_shape = (
+        # Visibility axis
+        # Number of visibilities in the data vector
+        BM.nbls*BM.nf*BM.nt,
+        # Sky model axis
+        # Number of HEALPix pixels in the model FoV across all frequencies
+        BM.hpx.npix_fov*BM.nf
+    )
+
+    assert Finv.shape == Finv_shape
+
+def test_build_Fprime(build_matrices):
+    BM = build_matrices
+
+    BM.build_Fprime()
+    Fprime = BM.read_data(BM.array_save_directory, "Fprime")
+
+    Fprime_shape = (
+        # Sky model axis
+        # Number of HEALPix pixels in the model FoV across all frequencies
+        BM.hpx.npix_fov*BM.nf,
+        # Combined EoR + FG model (u, v, f) cube axis
+        # Number of EoR model parameters
+        (BM.nu*BM.nv - 1)*BM.nf
+        # Number of FG model parameters
+        + (BM.nu_fg*BM.nv_fg - 1 + BM.fit_for_monopole)*BM.nf
+    )
+
+    assert Fprime.shape == Fprime_shape
+
+def test_build_T(build_matrices):
+    BM = build_matrices
+
+    # Ensure that pre-requisite matrices are built
+    BM.build_Finv()
+    BM.build_Fprime_Fz()
+
+    # Test build_T with a single thread
+    with threadpool_limits(limits=1):
+        BM.build_T()
+    T_single = BM.read_data(BM.array_save_directory, "T")
+
+    T_shape = (
+        # Visibility axis
+        # Number of visibilities in the data vector
+        BM.nbls*BM.nf*BM.nt,
+        # Combined EoR + FG model (u, v, eta) cube axis
+        # Number of EoR model parameters
+        (BM.nu*BM.nv - 1)*(BM.neta - 1)
+        # Number of eta=0 FG model parameters accounting
+        # for the Large Spectral Scale Model (LSSM)
+        + BM.nu_fg*BM.nv_fg*(1 + BM.nq)
+        # Number of (u, v)=(0, 0) monopole model parameters
+        + (BM.neta - 1)*BM.fit_for_monopole
+    )
+
+    assert T_single.shape == T_shape
+
+    Nthreads = threadpool_info["num_threads"]
+    if Nthreads > 1:
+        BM.build_T()
+        T_multi = BM.read_data(BM.array_save_directory, "T")
+
+        assert T_multi.shape == T_shape
+
+        # Ensure that T is identical with or without threading
+        diff = T_single - T_multi
+        assert np.abs(diff).max() <= 1e-12
