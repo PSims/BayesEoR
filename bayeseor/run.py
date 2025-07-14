1b33e4bc7ba5943251a3fd3e66d06ebdb6470c05
jburba <jacob.burba@manchester.ac.uk>
2025-07-14 17:43:40 +0100

    First attempt at new driver script

--- bayeseor/run.py
+++ bayeseor/run.py
@@ -1,12 +1,18 @@
 import numpy as np
+from collections.abc import Sequence
+from pathlib import Path
+from pymultinest.solve import solve
+from rich.panel import Panel
 
-from .posterior import PowerSpectrumPosteriorProbability
+from .posterior import PriorC, PowerSpectrumPosteriorProbability
 from .utils import mpiprint
 
 def run(
     *,
     pspp : PowerSpectrumPosteriorProbability,
-    out_dir : str = "./",
+    priors : Sequence[float],
+    n_live_points : int | None = None,
+    out_dir : Path | str = "./",
     sampler : str = "multinest",
     verbose : bool = False,
     rank : int = 0
@@ -17,8 +23,14 @@ def run(
     Parameters
     ----------
     pspp : :class:`bayeseor.posterior.PowerSpectrumPosteriorProbability`
-        Instantiated posterior class.
-    out_dir : str
+        Power spectrum posterior class.
+    priors : list of float
+        Prior [min, max] for each k bin as a list, e.g. [[min1, max1],
+        [min2, max2], ...].
+    n_live_points : int, optional
+        Number of live points. Defaults to None (sets the number of live
+        points to `25 * pspp.k_vals.size`).
+    out_dir : pathlib.Path or str, optional
         Sampler output directory. Must be less than 100 characters for
         compatibility with Multinest (the fortran code only supports a string
         with character length <= 100). It is encouraged you run BayesEoR from
@@ -33,31 +45,38 @@ def run(
         MPI rank. Defaults to 0.
 
     """
-    required_kwargs = [pspp, out_dir, sampler]
-    if not np.all([arg is not None for arg in required_kwargs]):
-        raise ValueError(
-            "The following kwargs are required and cannot be None: "
-            "pspp, out_dir, sampler"
-        )
+    if not isinstance(out_dir, Path):
+        out_dir = Path(out_dir)
+    out_dir.mkdir(parents=True, exist_ok=True)
+    sampler_output_base = (out_dir / "data-").as_posix()
 
     sampler = sampler.lower()
     if sampler == "multinest":
+        assert len(out_dir.as_posix()) <= 100, (
+            "When using MultiNest, the path to the sampler output directory "
+            "`out_dir` must be <= 100 characters in length"
+        )
+        # Log-likelihood wrapper function for MultiNest
         def loglikelihood(theta, calc_likelihood=pspp.posterior_probability):
             return calc_likelihood(theta)[0]
     elif sampler == "polychord":
-        raise NotImplementedError(
-            "PolyChord will be supported in the future."
-        )
+        raise NotImplementedError("PolyChord will be supported in the future.")
     else:
         raise ValueError("sampler must be one of 'multinest' or 'polychord'")
-    
-    # print_rank will only trigger print if verbose is True and rank == 0
-    print_rank = 1 - (verbose and rank == 0)
 
-    mpiprint(
-        "\nRunning power spectrum analysis...",
-        style="bold",
-        justify="center",
-        rank=rank,
-        end="\n\n"
-    )
+    nkbins = pspp.k_vals.size
+    if n_live_points is None:
+        n_live_points = 25 * nkbins
+    prior_c = PriorC(priors)
+    pspp.verbose = verbose
+
+    mpiprint("\n", Panel("Analysis"), rank=rank)
+    if sampler == "multinest":
+        result = solve(
+            LogLikelihood=loglikelihood,
+            Prior=prior_c,
+            n_dims=nkbins,
+            outputfiles_basename=sampler_output_base,
+            n_live_points=n_live_points
+        )
+    mpiprint("\nSampling complete!", rank=rank, end="\n\n")
